31714d5d0c97b5443756a29a160802817a67aac6
Pravin Roka <pravinrokamagar86@gmail.com>
2025-07-14 14:20:39 +0545

    .env sectet key change

--- projectcore/settings.py
+++ projectcore/settings.py
@@ -27,7 +27,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SCRIPT_KEY')    
+SECRET_KEY = os.getenv('SECRET_KEY')   
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
